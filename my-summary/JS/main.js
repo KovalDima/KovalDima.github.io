9443b778724d2c08821dbad350b3bc38379597c9
KovalDima <mr.smith8263@gmail.com>
2020-02-20 22:12:15 +0200

    add changes to index.html

--- my-summary/JS/main.js
+++ my-summary/JS/main.js
@@ -1,45 +1,58 @@
-jQuery(function ($) {
-    $('a[href*="#"]').on('click.smoothscroll', function (e) {
-        var hash = this.hash, _hash = hash.replace(/#/, ''), theHref = $(this).attr('href').replace(/#.*/, '');
+jQuery(function($) {
+  $('a[href*="#"]').on("click.smoothscroll", function(e) {
+    var hash = this.hash,
+      _hash = hash.replace(/#/, ""),
+      theHref = $(this)
+        .attr("href")
+        .replace(/#.*/, "");
 
-        if (theHref && location.href.replace(/#.*/, '') != theHref) return;
+    if (theHref && location.href.replace(/#.*/, "") != theHref) return;
 
-        var $target = _hash === '' ? $('body') : $(hash + ', a[name="' + _hash + '"]').first();
-        if (!$target.length) return;
+    var $target =
+      _hash === "" ? $("body") : $(hash + ', a[name="' + _hash + '"]').first();
+    if (!$target.length) return;
 
-        e.preventDefault();
+    e.preventDefault();
 
-        $('html, body').stop().animate({ scrollTop: $target.offset().top - 0 }, 800, 'swing', function () {
-            window.location.hash = hash;
-        });
-    });
+    $("html, body")
+      .stop()
+      .animate(
+        { scrollTop: $target.offset().top - 0 },
+        800,
+        "swing",
+        function() {
+          window.location.hash = hash;
+        }
+      );
+  });
 });
 
-function toggleFunction() {
-    let div = document.getElementById('js-toggle_block');
-    if (div.style.display === 'block') {
-        div.style.display = 'none';
-    } else {
-        div.style.display = 'block';
-    }
-};
+function toggleDropDown() {
+  let div = document.getElementById("js-toggle_block");
+  if (div.style.display === "block") {
+    div.style.display = "none";
+  } else {
+    div.style.display = "block";
+  }
+}
 
-$(document).ready(function () {
-    if ($('body').width() >= 1200) {
-        const checkOpened = () => $('.js-closed_container').hasClass('js-open_container');
-        const toggleOpen = () => {
-            $('.js-closed_container').toggleClass('js-open_container');
-            $('.js-portfolio').toggleClass('js-portfolio_origin');
-        }
+$(document).ready(function() {
+  if ($("body").width() >= 1200) {
+    const checkOpened = () =>
+      $(".js-closed_container").hasClass("js-open_container");
+    const toggleOpen = () => {
+      $(".js-closed_container").toggleClass("js-open_container");
+      $(".js-portfolio").toggleClass("js-portfolio_origin");
+    };
 
-        $('.js-card').on('click', toggleOpen);
+    $(".js-card").on("click", toggleOpen);
 
-        $('.js-cardLink').on('click', (e) => {
-            const isOpened = checkOpened();
+    $(".js-cardLink").on("click", e => {
+      const isOpened = checkOpened();
 
-            if (!isOpened) {
-                e.preventDefault();
-            }
-        })
-    }
+      if (!isOpened) {
+        e.preventDefault();
+      }
+    });
+  }
 });
