7ab70d1754b92610f137472ba807390706512c03
KovalDima <mr.smith8263@gmail.com>
2020-03-15 20:56:49 +0200

    add react site

---
+++ react-example-site/precache-manifest.d17a4e5575e5f04bd0f4c40eac0988b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58901cd9080e80999518e1c572de03e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd915f76dcaaf4a650d5",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "495108db22cbe42371c3",
+    "url": "/static/css/main.f22a5d5e.chunk.css"
+  },
+  {
+    "revision": "dd915f76dcaaf4a650d5",
+    "url": "/static/js/2.6ca7d7f9.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.6ca7d7f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495108db22cbe42371c3",
+    "url": "/static/js/main.5ea89611.chunk.js"
+  },
+  {
+    "revision": "8e2b64bb10f24d082d51",
+    "url": "/static/js/runtime-main.6651ead6.js"
+  },
+  {
+    "revision": "6a4c4eef1c9ed373b0afdabf7addc15b",
+    "url": "/static/media/nature-forest-trees-fog-4827.6a4c4eef.jpg"
+  },
+  {
+    "revision": "1ac195700f60682f1ed979c1ac9a2bff",
+    "url": "/static/media/seashore-and-mountain-3067769.1ac19570.jpg"
+  },
+  {
+    "revision": "32059243fa1a9e529e5b93e31e6debef",
+    "url": "/static/media/worms-eyeview-of-green-trees-957024.32059243.jpg"
+  }
+]);
